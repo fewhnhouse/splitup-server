8d349f3c29a215878a9ee0b3e984092dabf11077
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2018-09-08 12:47:38 +0200

    code improvements and fixes for nested remove statements.

--- src/resolvers/Mutation/group.js
+++ src/resolvers/Mutation/group.js
@@ -1,21 +1,5 @@
 const { getUserId } = require("../../utils");
 
-const query = `
-  {
-    participants {
-      id
-    }
-  }
-`;
-
-async function getParticipants(ctx, groupId) {
-  const group = await ctx.db.query.group({ where: { id: groupId } }, query);
-  if (group === null) {
-    throw new Error("Group does not exist.");
-  }
-  return group.participants.map(participant => ({ id: participant.id }));
-}
-
 const group = {
   async createGroup(parent, { input }, ctx, info) {
     const { title, participants } = input;
@@ -27,9 +11,11 @@ const group = {
         data: {
           title,
           participants: {
-            connect: participants.map(el => {
-              return { id: el };
-            })
+            set: {
+              connect: participants.map(el => {
+                return { id: el };
+              })
+            }
           },
           author: {
             connect: { id: userId }
@@ -41,47 +27,28 @@ const group = {
   },
   async joinGroup(parent, { id }, ctx, info) {
     const userId = getUserId(ctx);
-    let participants = await getParticipants(ctx, id);
-    console.log("initial participants: ", participants);
-    if (participants.includes(userId)) {
-      throw new Error("User is already a member of the group.");
-    } else {
-      participants.push({ id: userId });
-    }
-    console.log("final participants: ", participants);
 
     return ctx.db.mutation.updateGroup(
       {
         where: { id },
-        data: { participants: { connect: participants } }
+        data: { participants: { connect: { id: userId } } }
       },
       info
     );
   },
   async leaveGroup(parent, { id }, ctx, info) {
     const userId = getUserId(ctx);
-    const participants = await getParticipants(ctx, id);
-    if (!participants.find(participant => participant.id === userId)) {
-      throw new Error(`User not found on group`);
-    }
-
-    const newParticipants = participants.filter(
-      participant => participant.id !== userId
+    return ctx.db.mutation.updateGroup(
+      {
+        where: { id },
+        data: { participants: { disconnect: { id: userId } } }
+      },
+      info
     );
-
-    console.log("participants: ", participants);
-
-    console.log("newParticipants:", newParticipants);
-    return ctx.db.mutation.updateGroup({
-      where: { id },
-      data: { participants: { connect: newParticipants } }
-    });
   },
-  async modifyGroup(parent, { input }, ctx, info) {
-    console.log("args:", input);
-    const { groupId, title, participants } = input;
+  async modifyGroupTitle(parent, { id, title }, ctx, info) {
     const groupExists = await ctx.db.exists.Group({
-      id: groupId
+      id
     });
     if (!groupExists) {
       throw new Error("Group does not exist");
@@ -89,10 +56,31 @@ const group = {
 
     return ctx.db.mutation.updateGroup(
       {
-        where: { id: groupId },
+        where: { id },
         data: {
-          title,
-          participants
+          title
+        }
+      },
+      info
+    );
+  },
+  async modifyGroupParticipants(parent, { input }, ctx, info) {
+    const { add, participants, groupId } = input;
+    const groupExists = await ctx.db.exists.Group({
+      id: groupId
+    });
+    if (!groupExists) {
+      throw new Error("Group does not exist");
+    }
+
+    const mappedParticipants = participants.map(el => ({ id: el }));
+    return ctx.db.mutation.updateGroup(
+      {
+        where: { id },
+        data: {
+          participants: add
+            ? { connect: mappedParticipants }
+            : { disconnect: mappedParticipants }
         }
       },
       info
--- src/resolvers/Mutation/user.js
+++ src/resolvers/Mutation/user.js
@@ -1,60 +1,26 @@
 const { getUserId } = require("../../utils");
 
-const query = `
-  {
-    id
-    name
-    friends {
-      name
-      id
-    }
-  }
-`;
-
-async function getOwnFriends(ctx, userId) {
-  const me = await ctx.db.query.user({ where: { id: userId } }, query);
-  return me.friends.map(friend => ({ id: friend.id }));
-}
-
 const user = {
   async addFriend(parent, { id }, ctx, info) {
-    const friendExists = await ctx.db.exists.User({
-      id
-    });
-    if (!friendExists) {
-      throw new Error("Friend does not exist");
-    }
     const userId = await getUserId(ctx);
-    let friends = await getOwnFriends(ctx, userId);
-    if (friends.includes(id)) {
-      throw new Error("Friend already exists");
-    } else if (id === userId) {
+    if (id === userId) {
       throw new Error("Can´t add yourself to friends");
-    } else {
-      friends.push({ id });
     }
 
     return ctx.db.mutation.updateUser(
       {
         where: { id: userId },
-        data: { friends: { connect: friends } }
+        data: { friends: { connect: { id } } }
       },
       info
     );
   },
   async removeFriend(parent, { id }, ctx, info) {
     const userId = await getUserId(ctx);
-    let friends = await getOwnFriends(ctx, userId);
-    if (!friends.find(friend => friend.id === id)) {
-      throw new Error(`Friend not found on user`);
-    }
-
-    const newFriends = friends.filter(friend => friend.id !== id);
-    console.log(newFriends);
     return ctx.db.mutation.updateUser(
       {
         where: { id: userId },
-        data: { friends: { connect: newFriends } }
+        data: { friends: { disconnect: { id } } }
       },
       info
     );
