037dff7e8a5bc891175de46c2cb37cbebf3dea9e
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2018-09-11 18:03:58 +0200

    First attempt at upload storage.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const typeDefs = require("./typeDefs");
 
 const db = new Prisma({
   typeDefs: "src/generated/prisma.graphql", // the auto-generated GraphQL schema of the Prisma API
-  endpoint: "http://localhost:4466" // the endpoint of the Prisma API (value set in `.env`)
-  //debug: true // log all GraphQL queries & mutations sent to the Prisma API
+  endpoint: "http://localhost:4466", // the endpoint of the Prisma API (value set in `.env`)
+  debug: true // log all GraphQL queries & mutations sent to the Prisma API
   // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
 });
 
--- src/resolvers/Mutation/user.js
+++ src/resolvers/Mutation/user.js
@@ -1,4 +1,36 @@
 const { getUserId } = require("../../utils");
+const path = require("path");
+const mkdirp = require("mkdirp");
+const fs = require("fs");
+const shortid = require("shortid");
+
+const uploadDir = path.resolve("uploads/");
+mkdirp.sync(uploadDir);
+
+const storeFS = ({ stream, filename }) => {
+  const id = shortid.generate();
+  const path = `${uploadDir}/${id}-${filename}`;
+  console.log;
+  return new Promise((resolve, reject) =>
+    stream
+      .on("error", error => {
+        if (stream.truncated)
+          // Delete the truncated file
+          fs.unlinkSync(path);
+        reject(error);
+      })
+      .pipe(fs.createWriteStream(path))
+      .on("error", error => reject(error))
+      .on("finish", () => resolve({ id, path }))
+  );
+};
+
+const base64_encode = file => {
+  // read binary data
+  var bitmap = fs.readFileSync(file);
+  // convert binary data to base64 encoded string
+  return new Buffer(bitmap).toString("base64");
+};
 
 const user = {
   async addFriend(parent, { id }, ctx, info) {
@@ -16,11 +48,16 @@ const user = {
     );
   },
   async addAvatar(parent, { file }, ctx, info) {
+    const { stream, filename, mimetype, encoding } = await file;
+    const { id, path } = await storeFS({ stream, filename });
+    const base64Img = base64_encode(path);
+    var bitmap = fs.readFileSync(file);
+
     const userId = await getUserId(ctx);
     return ctx.db.mutation.updateUser(
       {
         where: { id: userId },
-        data: { avatar: file }
+        data: { avatar: bitmap }
       },
       info
     );
